d983a46fc878df60804fb65e238d9bfa8f21f37b
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-04-12 13:00:35 +0530

    Added Functionality to search bar.

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,27 +1,47 @@
-import React, { useContext } from 'react'
-import EachNote from './EachNote'
-// import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import EachNote from './EachNote';
 import NoteContext from '../Contexts/NoteContext';
 import FloatingButton from './FloatingButton';
 
 export default function HomePage() {
-  const {notes} = useContext(NoteContext)
-  return (
-    <div className='main-cont'>
-      <div className='main-upper-cont'>
-        <h1 className='title'>SnapThought</h1>
-        
-      </div>
-      <input type='search' placeholder='Search Notes' className='input-search'/>
-      {notes.map((note)=>{
-        return (<EachNote key={note.id} note={note}/>)
-      })}
-      <FloatingButton/>
-      
+    const { notes } = useContext(NoteContext);
+    const [searchQuery, setSearchQuery] = useState('');
 
-      
-      
-    </div>
+   
+    const filteredNotes = notes.filter(note => 
+        note.title.toLowerCase().includes(searchQuery.toLowerCase()) || 
+        note.description.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
     
-  )
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    return (
+        <div className='main-cont'>
+            <div className='main-upper-cont'>
+                <h1 className='title'>SnapThought</h1>
+            </div>
+            
+            <input 
+                type='search' 
+                placeholder='Search Notes' 
+                className='input-search' 
+                value={searchQuery} 
+                onChange={handleSearchChange} 
+            />
+            
+           
+            {filteredNotes.length > 0 ? (
+                filteredNotes.map((note) => (
+                    <EachNote key={note.id} note={note} />
+                ))
+            ) : (
+                <div className='no-results-message'>No results found</div>
+            )}
+            
+            <FloatingButton />
+        </div>
+    );
 }
